275c71d7f2efc5c3e8d0c02d377c51e223d379c2
typoworx-de <info@typoworx.com>
2020-01-18 21:01:17 +0100

    Update ubuntu-lightweight-bootstrap.sh

--- usr/local/bin/ubuntu-lightweight-bootstrap.sh
+++ usr/local/bin/ubuntu-lightweight-bootstrap.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-apt-get remove -y --purge accountsservice language-selector-common;
+
+# Check for VMware Hypervisor
+if [[ $(dmesg | grep -c 'VMware') > 0 ]];
+then
+  apt-get install -y open-vm-tools;
+fi
+
+# Check for Debian Linux
+if [[ $(grep -c 'Debian' /etc/issue) > 0 ]];
+then
+  apt-get remove -y --purge accountsservice language-selector-common;
+fi
