7f9952d5f130aa87f1e382decfe42e3ce80fb670
typoworx-de <info@typoworx.com>
2020-09-30 15:36:25 +0200

    Update ubuntu-lightweight-bootstrap.sh

--- usr/local/bin/ubuntu-lightweight-bootstrap.sh
+++ usr/local/bin/ubuntu-lightweight-bootstrap.sh
@@ -9,8 +9,10 @@ fi
 # Check for Debian Linux
 if [[ $(grep -c 'Debian' /etc/issue) > 0 ]];
 then
-  apt-get remove -y --purge accountsservice language-selector-common ubuntu-standard;
+  apt-get remove -y --purge accountsservice language-selector-common;
 fi
+
+apt-get remove -y --purge ubuntu-standard;
 
 # Generic packages assumed essential
 apt-get install curl apt-transport-https;
