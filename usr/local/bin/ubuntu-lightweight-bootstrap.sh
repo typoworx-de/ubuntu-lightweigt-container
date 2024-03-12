a55d1a1f1e8c2a7b4a134d7583690533af66eed2
typoworx-de <info@typoworx.de>
2024-03-12 15:55:15 +0100

    Update ubuntu-lightweight-bootstrap.sh

--- usr/local/bin/ubuntu-lightweight-bootstrap.sh
+++ usr/local/bin/ubuntu-lightweight-bootstrap.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+declare -a locales=(de_DE.UTF-8 en_US.UTF-8);
+declare _repoRelPath=$(dirname $0)'/../../..';
+
+if [ "$EUID" -ne 0 ]; then
+  echo "root permissions required!"
+  exit 255;
+fi
+
+cp -R ${_repoRelPath}/etc/* /etc/;
+rm -rf /usr/share/doc/*;
+rm -rf /usr/share/man/*;
+rm -rf /usr/share/locale/*;
 
 apt update || exit 1;
-
-apt-get install apt-get;
+apt-get install apt-get safe-rm;
 
 # Check for VMware Hypervisor
 if [[ $(dmesg | grep -c 'VMware') > 0 ]];
@@ -34,3 +45,9 @@ apt-get clean
 # Create new host-keys
 rm -f /etc/ssh/ssh_host_* || true
 dpkg-reconfigure openssh-server
+
+/usr/sbin/locale-gen --purge "${locales[@]}" || true;
+
+# Beautify /etc/fstab by tabbed-format
+[ ! -f /etc/fstab.dist ] && cp /etc/fstab /etc/fstab.dist;
+cat < /etc/fstab | awk '/^#/ && !/# <file system>/ { next } NF == 0 { next } {gsub(/<\s+/, "<", $0)}; { printf "%-20s%-20s%-20s%-10s%-10s%-20s\n", $1, $2, $3, $4, $5, $6 }' | tee /etc/fstab;
