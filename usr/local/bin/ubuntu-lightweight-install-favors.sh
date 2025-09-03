7d08ebdd4f8d8735d13bb5371555310f5406754b
typoworx-de <info@typoworx.de>
2025-09-03 10:44:52 +0200

    Update ubuntu-lightweight-install-favors.sh

--- usr/local/bin/ubuntu-lightweight-install-favors.sh
+++ usr/local/bin/ubuntu-lightweight-install-favors.sh
@@ -2,11 +2,14 @@
 
 sudo apt-get install --no-install-recommends \
   aptitude net-tools \
-  chkrootkit msmtp-mta
+  rkhunter msmtp-mta \
 ;
 
 sudo touch /etc/msmtprc
 sudo chmod 600 /etc/msmtprc
+
+sudo rkhunter --update
+sudo rkhunter --propupd
 
 echo "Please configure Mail-MTA /etc/msmtprc"
 echo "done"
